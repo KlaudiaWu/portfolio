2509c01bd0a33146fad5181e4a7e63902250234a
Klaudia <wojciak.klaudia96@gmail.com>
2019-04-04 22:03:08 +0200

    Progress in portfolio slider (moving works)

--- js/portfolio-section.js
+++ js/portfolio-section.js
@@ -1,89 +1,121 @@
-(function() {
+(function () {
 
 
     var elem = Array.from(document.getElementsByClassName("project"));
     var nextSection = document.getElementsByClassName("about-me-section");
 
     console.log("Tablica: " + elem);
+    var offsetValues = new Array(elem.length).fill(0);
+    console.log(offsetValues);
 
 
 
-// scroll bound animation !!!
+    // scroll bound animation !!!
 
 
     var offTop;
     ticking = false;
     offTop = elem[0].getBoundingClientRect().top; //Pierwszy element triggeruje rozpoczęcie scrolla horyzontalnego
 
-
-    window.onscroll = function() {
+    window.onscroll = function () {
         offTop = elem[0].getBoundingClientRect().top; //Pierwszy element triggeruje rozpoczęcie scrolla horyzontalnego
-
-
-        if(offTop <= 0) {
+        if (offTop <= 0) {
             document.body.style.overflowY = "hidden";
         }
 
     };
+    var start = 0;
+    var index = 0;
+    var globalMove = 15;
+    var change = 0;
 
-    var somePixels = 0;
-    window.onwheel = function(e) {
-        
-        if(e.deltaY > 0) {
-            somePixels += 5;
-            console.log("trochę tak do przodu " + somePixels);
-        } else {
-            somePixels -= 5;
-            console.log("trochę tak do tyłu " + somePixels);
-        }
-        
-        requestTick();
-    };
+    count(0);
 
-    function requestTick() {
-        if(!ticking) {
-            requestAnimationFrame(horizontalScroll);
-        }
-        ticking = true;
+    function count(index) {
+        index = index;
+        window.onwheel = function (e) {
+            console.log(start++);
+            if (e.deltaY > 0) {
+                for (i = 0; i < index + 1; i++) {
+                    offsetValues[i] += globalMove;
+                }
+                console.log("trochę tak do przodu " + offsetValues[index]);
+            } else {
+                for (i = 0; i < index + 1; i++) {
+                    offsetValues[i] -= globalMove;
+                }
+                console.log("trochę tak do tyłu " + offsetValues[index]);
+            }
+
+            requestTick();
+        };
     }
 
-    function horizontalScroll () {
-        
-        // reset the tick so we can
-	    // capture the next onScroll
-	    ticking = false;
+    function requestOffsetValue(index) {
+        return offsetValues[index];
+    }
 
+    function requestTick() {
+        if (!ticking) {
+            requestAnimationFrame(horizontalScroll);
+        }
+    }
+
+    var setActive = false;
+
+    function horizontalScroll() {
+
+        // reset the tick so we can
+        // capture the next onScroll
 
         //console.log(offTop);
 
         offTop = elem[0].getBoundingClientRect().top; //Pierwszy element triggeruje rozpoczęcie scrolla horyzontalnego
+        if (offTop <= 0) {
 
-        if(offTop <= 0) {
+            for (i = 0; i < elem.length; i++) {
+                //Muszę ozmaczyć element aktywny
+                var currentScrollY = requestOffsetValue(i);
 
-            
-            var currentScrollY = somePixels;
-            elem[0].style.transform = "translateX(" + - currentScrollY + "px)";
-            var offLeft = elem[0].getBoundingClientRect().left;
-
-            if(elem[0].getBoundingClientRect().right === elem[1].getBoundingClientRect().left) {
-                //Musisz jakoś zresetować wartość currentScrollY
-                elem[1].style.transform = "translateX(" + - currentScrollY + "px)";
+                for (j = 1; j <= globalMove; j++) {
+                    if (elem[i].getBoundingClientRect().right <= (elem[i + 1].getBoundingClientRect().left + j)) {
+                        count(i + 1);
+                        break;
+                    } else {
+                        elem[i].style.transform = "translateX(" + -currentScrollY + "px)";
+                    }
+                }
             }
-
-            //for(i = 0; i <= elem.length; i++) {
-
-                //console.log("Lewa krawędź: " + elem[i+1].getBoundingClientRect().left);
-                //console.log("Prawa krawędź: " + elem[i].getBoundingClientRect().right);
-                //console.log("Pokaż mi mój element " + elem[i]);
-
-            //}
         }
 
+        ticking = false;
 
-        requestAnimationFrame(horizontalScroll);
+        // requestAnimationFrame(horizontalScroll);
         // https://www.html5rocks.com/en/tutorials/speed/animations/
 
     }
 
+    function addClass(el, className) {
+        if (el.classList)
+            el.classList.add(className);
+        else if (!hasClass(el, className))
+            el.className += " " + className;
+    }
 
-})(); 
+    function hasClass(el, className) {
+        if (el.classList)
+            return el.classList.contains(className);
+        return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+    }
+
+    function removeClass(el, className) {
+        if (el.classList)
+            el.classList.remove(className);
+        else if (hasClass(el, className)) {
+            var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+            el.className = el.className.replace(reg, ' ');
+        }
+    }
+
+
+})();
