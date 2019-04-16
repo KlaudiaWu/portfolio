f7f496e64cfe21be58a68764002a50c4c0fa281d
Klaudia <wojciak.klaudia96@gmail.com>
2019-04-16 23:51:46 +0200

    portfolio: restored scroll (onscroll instead of onwhell)

--- js/backup.js
+++ js/backup.js
@@ -1,26 +1,11 @@
 (function () {
 
-    
 
-    var sliderWrapper = document.getElementsByClassName("slider-wrapper");
     var elem = Array.from(document.getElementsByClassName("project"));
     var furtherElem = Array.from(document.getElementsByClassName("after-slider"));
     elem = elem.concat(furtherElem);
 
-    var scrollWidth = 0;
-    for (i = 0; i < elem.length; i++) {
-        console.log(getComputedStyle(elem[i]).height);
-        elem[i].style.height = getComputedStyle(elem[i]).height;
-        elem[i].style.width = getComputedStyle(elem[i]).width;
-       scrollWidth += parseInt(getComputedStyle(elem[i]).width);
-    }
-    scrollWidth = scrollWidth + "px";
-
-    var scrollPath = document.getElementsByClassName("scroll-container");
-    scrollPath[0].style.height = scrollWidth;
-
-    console.log(getComputedStyle(scrollPath[0]).height);
-    console.log(scrollWidth);
+    console.log(furtherElem);
 
     var width = window.width;
     var offsetLeft = parseInt(getComputedStyle(elem[1]).left);
@@ -48,35 +33,37 @@
 
     var offTop;
     ticking = false;
+    offTop = elem[0].getBoundingClientRect().top; //Pierwszy element triggeruje rozpoczęcie scrolla horyzontalnego
 
     window.onscroll = function () {
+        offTop = elem[0].getBoundingClientRect().top; //Pierwszy element triggeruje rozpoczęcie scrolla horyzontalnego
+        if (offTop <= 0) {
+            document.body.style.overflowY = "hidden";
+        }
 
     };
     var start = 0;
     var index = 0;
-    var globalMove = 0;
+    var globalMove = 15;
     var change = 0;
-    var prevScroll = 0;
 
     count(0);
 
     function count(index) {
         index = index;
-        window.onscroll = function (e) {
-            offTop = scrollPath[0].getBoundingClientRect().top; //Pierwszy element triggeruje rozpoczęcie scrolla horyzontalnego
-            if (offTop <= 0) {
-                sliderWrapper[0].style.height = getComputedStyle(sliderWrapper[0]).height;
-                sliderWrapper[0].style.width = getComputedStyle(sliderWrapper[0]).width;
-                sliderWrapper[0].style.position = "fixed";
-                sliderWrapper[0].style.top = 0;
-                sliderWrapper[0].style.left = 0;
+        window.onwheel = function (e) {
+            if (e.deltaY > 0) {
                 for (i = 0; i < index + 1; i++) {
                     offsetValues[i] += globalMove;
                 }
+                console.log("trochę tak do przodu " + offsetValues[index]);
+            } else {
+                for (i = 0; i < index + 1; i++) {
+                    offsetValues[i] -= globalMove;
+                }
+                console.log("trochę tak do tyłu " + offsetValues[index]);
             }
-            globalMove = window.scrollY - prevScroll;
-            prevScroll = window.scrollY;
-                
+
             requestTick();
         };
     }
@@ -95,6 +82,7 @@
     function horizontalScroll() {
 
 
+        offTop = elem[0].getBoundingClientRect().top; //Pierwszy element triggeruje rozpoczęcie scrolla horyzontalnego
         stap:
         if (offTop <= 0) {
             for (i = 0; i < elem.length; i++) {
@@ -108,6 +96,7 @@
                     if (i === offsetValues.length - 2 && offsetValues[i] > 0) {
                         elem[i].style.transform = "translateX(" + -currentScrollY + "px)";
                         elem[i+1].style.transform = "translateX(" + -currentScrollY + "px)";
+                        console.log(getComputedStyle(furtherElem.style.left));
                         //if (getComputedStyle(furtherElem.style.left) === 0) {
                         //   console.log("Dupa");
                         //}
