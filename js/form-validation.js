9935464e96e87f7e3ba9b9b8645f1f91618f7986
Klaudia <wojciak.klaudia96@gmail.com>
2019-07-22 17:48:16 +0200

    latest release

---
+++ js/form-validation.js
@@ -0,0 +1,11 @@
+// 1. Check if inputs are not empty
+// 2. Check if email is correct
+// 3. Check if re-captcha return true
+// 4. Send information
+
+var form = document.getElementById("contact-form");
+
+
+form.addEventListener("submit", function validate() {
+
+});
